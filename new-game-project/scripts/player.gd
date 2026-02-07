44f18cc665430ef8519eee9e72e6deb12c8ba82c
unknown <parnikab127@gmail.com>
2026-02-07 13:01:46 +0530

    added animations and basic tilemap

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,14 +1,15 @@
-eextends CharacterBody2D
+extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -100.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * _delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
